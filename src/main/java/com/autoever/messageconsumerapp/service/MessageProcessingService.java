871a3a14f3add8349f34eef63fedb883f621b469
moon-mac <bbangssi91@gmail.com>
2025-08-04 00:33:53 +0900

    [refactor] - API키 application.yml로 이동 - API 호출 실패 시, 예외수정 - fallback 메서드 분리

--- src/main/java/com/autoever/messageconsumerapp/service/MessageProcessingService.java
+++ src/main/java/com/autoever/messageconsumerapp/service/MessageProcessingService.java
@@ -7,9 +7,10 @@ import com.autoever.messageconsumerapp.dto.request.KakaoMessageRequestDto;
 import com.autoever.messageconsumerapp.external.KakaoMessageClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -19,32 +20,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageProcessingService {
 
-    private final String API_KEY = "asdfjalksdfjalksdfjalksdwgaegew";
+    @Value( "${external.kakao.api-key}")
+    private String API_KEY;
 
-    private final RedisTemplate<String, String> redisTemplate;
     private final KakaoMessageClient kakaoMessageClient;
     private final MessageLogService messageLogService;
     private final RedisPublisher redisPublisher;
-    private final ObjectMapper objectMapper;
 
     @Async("asyncTaskExecutor")
     public void processMessageAsync(MessageLogsVO message) throws JsonProcessingException {
         log.info("[Message Processing] Message Processing Start");
-        KakaoMessageRequestDto kakaoMessageRequestDto = new KakaoMessageRequestDto(message.getPhone(), message.getContent());
-        // 1. 카카오메시지 API를 호출한다
-        ResponseEntity<?> response = kakaoMessageClient.sendKakaoMessage(API_KEY, kakaoMessageRequestDto);
-        log.info("[Message Processing] Call API End {}", response.getStatusCode());
 
-        boolean isSuccess = response.getStatusCode().is2xxSuccessful();
+        try {
+            // 1. 카카오메시지 API를 호출한다
+            KakaoMessageRequestDto kakaoMessageRequestDto = new KakaoMessageRequestDto(message.getPhone(), message.getContent());
+            ResponseEntity<?> response = kakaoMessageClient.sendKakaoMessage(API_KEY, kakaoMessageRequestDto);
 
-        // 2. HttpStatusCode 성공 결과에 따라, DB 상태를 update 한다
-        messageLogService.updateStatus(message, isSuccess);
+            // 2. HttpStatusCode 성공 결과에 따라, DB 상태를 update 한다
+            messageLogService.updateStatus(message, response.getStatusCode().is2xxSuccessful());
 
-        // 3. 메시지 성공 실패일 경우, 별도로 SMS 전송 이벤트를 발행한다
-        if(!isSuccess) {
-            String json = objectMapper.writeValueAsString(message);
-            redisPublisher.publish(MessageChannel.SMS_CHANNEL.getChannelName(), json);
+        } catch (FeignException e) {
+            log.error("[Message Processing] FeignException occurred: {}", e.getMessage(), e);
+            // API 호출 예외 발생 시, 상태를 실패로 update 한다
+            messageLogService.updateStatus(message, e.status() == 200);
+            // 메시지 성공 실패일 경우, fallback 으로 SMS 전송 이벤트를 발행한다
+            publishSmsFallbackEvent(message);
         }
+    }
 
+    private void publishSmsFallbackEvent(MessageLogsVO message) {
+        try {
+            String json = new ObjectMapper().writeValueAsString(message);
+            redisPublisher.publish(MessageChannel.SMS_CHANNEL.getChannelName(), json);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to serialize fallback message", e);
+        }
     }
 }
